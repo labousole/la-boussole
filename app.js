87c6d715d8207aea0ef3777f1b9f20c3a0baa538
labousole <rock.dylan@orange.fr>
2026-09-17 13:51:08 +0200

    Add 'Outils' tab to the main component

--- app.js
+++ app.js
@@ -882,6 +882,7 @@ function App() {
         {tab === "partis" && <Partis />}
         {tab === "candidats" && <Candidats />}
         {tab === "dossiers" && <Dossiers />}
+        {tab === "dossiers" && <Outils />}
         {tab === "comparateur" && <Comparateur />}
         {tab === "boussole" && <Boussole />}
         {tab === "edito" && <Edito />}
